98c140e93ae6ce1e026f76bbfd11c47a157099a8
Araceli <aracelirusa@gmail.com>
2025-11-11 17:07:26 -0700

    jeje

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -39,6 +39,7 @@ export const deletePost = async (req,res) => {
         `;
         const result =  await query(deletePostQuery,[id]);
         res.json(result.rows[0]);
+        res.status(200).json({ message: "Post deleted successfully" });
     }catch(error){
         res.status(400).json({error: error.message});
     }
